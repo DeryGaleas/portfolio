776f9015d17f0a58663e64a6d9d0f261cf7ab319
Dery Galeas <dery.reyna@gmail.com>
2024-01-15 10:56:47 -0600

    navbar name update 0.2

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import { HeroSection } from "./component/HeroSection";
-import { NavBar } from "./component/Navbar";
+import { Navbar } from "./component/Navbar";
 import { AboutSection } from "./component/AboutSection";
 import { ProjecsSection } from "./component/ProjecsSection";
 import { Footer } from "./component/Footer";
@@ -8,7 +8,7 @@ import { AchievementsSection } from "./component/AchievementsSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <NavBar />
+      <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AchievementsSection />
