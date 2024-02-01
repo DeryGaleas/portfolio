ab60b30d617e555e12248747a859f17fe1a5af02
Dery Galeas <dery.reyna@gmail.com>
2024-02-01 12:57:33 -0600

    Vercel bug fixed

--- app/component/AboutSection.jsx
+++ app/component/AboutSection.jsx
@@ -27,15 +27,16 @@ export const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            Hello everyone. I would like to introduce myself. I am Dery Galeas,
+            {`Hello everyone. I would like to introduce myself. I am Dery Galeas,
             a professional delighted to work in passionate teams in software
             development. My constant curiosity in the projects I have
             undertaken, has led me to embark on a new academic journey, by
             pursuing a Master's in Mathematical Engineering to gain a deeper
-            knowledge of the subject.
-            <br /> <br />I would love to help in any way possible. Feel free to
+            knowledge of the subject.`}
+            <br /> <br />
+            {`I would love to help in any way possible. Feel free to
             contact me or reach out for any questions or concerns. It will
-            always be a pleasure.
+            always be a pleasure.`}
           </p>
 
           <div className="flex flex-row mt-8 justify-start">
